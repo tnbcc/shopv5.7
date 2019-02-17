6015b2ed5b4a7a00ab7d94711919ff7998dc3efd
“朱聪” <tniub.cc@gmail.com>
2019-02-17 06:06:31 +0000

    使用redis作为session存储驱动

---
+++ app/Notifications/EmailVerificationNotification.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class EmailVerificationNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting($notifiable->name.'您好：')
+            ->subject('注册成功，请验证您的邮箱')
+            ->line('请点击下方链接验证您的邮箱')
+            ->action('验证', url('/'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
