5f450f1edab626b614332da80771e4d6bf083642
“朱聪” <tniub.cc@gmail.com>
2019-02-17 15:59:09 +0000

    用户界面 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,8 +9,38 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        $builder = Product::query()->where('on_sale', true);
 
-        return view('products.index', compact('products'));
+        if ($search = $request->input('search', '')) {
+            $like = '%'.$search.'%';
+            // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        if ($order = $request->input('order', '')) {
+
+            //是否以_asc 或者 desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+
+        $filters = [
+          'search' => $search,
+          'order'  => $order,
+        ];
+
+        return view('products.index', compact('products', 'filters'));
     }
 }
