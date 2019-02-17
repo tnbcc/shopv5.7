555480db06461b9695576c2d7091a4bc7b7ef994
“朱聪” <tniub.cc@gmail.com>
2019-02-17 07:34:29 +0000

    优雅的处理异常

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,5 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 
-    Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
-    });
-
 
 });
