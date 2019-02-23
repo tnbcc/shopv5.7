1ad7180ad7a994acc6d58804c21b915bf3d413f5
“朱聪” <tniub.cc@gmail.com>
2019-02-23 02:09:53 +0000

    调整

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -119,20 +119,6 @@ class ProductsController extends Controller
                         ->get();
 
 
-        // 创建一个查询构造器，只搜索上架的商品，取搜索结果的前 4 个商品
-        $builder = (new ProductSearchBuilder())->onSale()->paginate(4, 1);
-        // 遍历当前商品的属性
-        foreach ($product->properties as $property) {
-            // 添加到 should 条件中
-            $builder->propertyFilter($property->name, $property->value, 'should');
-        }
-        // 设置最少匹配一半属性
-        $builder->minShouldMatch(ceil(count($product->properties) / 2));
-        $params = $builder->getParams();
-        // 同时将当前商品的 ID 排除
-        $params['body']['query']['bool']['must_not'] = [['term' => ['_id' => $product->id]]];
-        // 搜索
-        $result = app('es')->search($params);
         $similarProductIds = $service->getSimilarProductIds($product, 4);
         // 根据 Elasticsearch 搜索出来的商品 ID 从数据库中读取商品数据
         $similar   = Product::query()->byIds($similarProductIds)->get();
